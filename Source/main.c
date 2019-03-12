8d6cda363512bb7a1f644838363e72fe28ec6cc8
Dmytro <44137092+KieeRt@users.noreply.github.com>
2019-03-12 12:33:14 +0100

    Create main.c

---
+++ Source/main.c
@@ -0,0 +1,26 @@
+#include "head.h"
+
+
+
+int main(){
+
+int mazzo[max][max];
+int tabella[max][max];
+
+
+/*genera_mazzo(mazzo);
+//stampa_mazzo(mazzo);
+crea_tabella(tabella);
+stampa_tabella(tabella);
+start(mazzo,tabella);
+*/
+leggi_dati(tabella);
+stampa_tabella(tabella);
+
+
+return 0 ;
+}
+
+
+
+
